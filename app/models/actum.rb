ff1cdbf6426d7c7466fedf4cce05b9f346a3711f
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2013-11-28 11:46:07 -0600

    unprotect the counter cache fields

--- app/models/actum.rb
+++ app/models/actum.rb
@@ -6,7 +6,7 @@ class Actum < ActiveRecord::Base
   validates :alianza, :dc, :liberal, :libre, :nacional, :pac, :pinu, :ud, :nulos, :blancos, :numericality => { :greater_than_or_equal_to=>0, :less_than_or_equal_to => 400 }, :presence => true
   
  
-  belongs_to :user, counter_cache: true
+  belongs_to :user #, counter_cache: true
   has_many :verifications, class_name: "Verification",:foreign_key=>"acta_id"
   
   
