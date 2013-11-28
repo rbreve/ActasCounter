7761506fa65857b3811e658bd61f2b11e75bc8ea
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2013-11-28 13:37:24 -0600

    pasar a folder 2 de s3

--- app/controllers/acta_controller.rb
+++ app/controllers/acta_controller.rb
@@ -44,7 +44,7 @@ class ActaController < ApplicationController
     end
     @totalVotos=@actum.nacional.to_i+@actum.liberal.to_i+@actum.libre.to_i+@actum.ud.to_i+@actum.alianza.to_i+@actum.pinu.to_i+@actum.blancos.to_i+@actum.pac.to_i+@actum.nulos.to_i+@actum.dc.to_i
     
-    @imageUrl = "http://s3-us-west-2.amazonaws.com/actashn/presidente/1/%05d.jpg" % @actum.numero
+    @imageUrl = "http://s3-us-west-2.amazonaws.com/actashn/presidente/2/%05d.jpg" % @actum.numero
     
     @verification=Verification.new
     @verification.is_valid=true
@@ -97,7 +97,7 @@ class ActaController < ApplicationController
           i = Random.rand(15000)
         end
 
-        @imageUrl = "http://s3-us-west-2.amazonaws.com/actashn/presidente/1/%05d.jpg" % i
+        @imageUrl = "http://s3-us-west-2.amazonaws.com/actashn/presidente/2/%05d.jpg" % i
  
         begin
           open(@imageUrl)
@@ -146,7 +146,7 @@ class ActaController < ApplicationController
           format.json { render json: @actum, status: :created, location: @actum }
         else
           i=@actum.numero.to_i
-          @imageUrl = "http://s3-us-west-2.amazonaws.com/actashn/presidente/1/%05d.jpg" % i
+          @imageUrl = "http://s3-us-west-2.amazonaws.com/actashn/presidente/2/%05d.jpg" % i
           
           format.html { render action: "new" }
           format.json { render json: @actum.errors, status: :unprocessable_entity }
