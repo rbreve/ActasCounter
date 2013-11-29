c9bf5988875b3a3be7e87219b240ad3d6ba2b057
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2013-11-28 23:47:47 -0600

    fixes

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -2,7 +2,7 @@ class ReportesController < ApplicationController
   # GET /reportes
   # GET /reportes.json
   def index
-    @reportes = Reporte.all
+    @reportes = Reporte.order("created_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/reporte.rb
+++ app/models/reporte.rb
@@ -1,4 +1,5 @@
 class Reporte < ActiveRecord::Base
   attr_accessible :acta_id, :body, :date, :user_id
   belongs_to :actum, class_name: "Actum",:foreign_key=>"acta_id"
+  belongs_to :user
 end
