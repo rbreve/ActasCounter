3522a88bc99769f449c15fa1d70ee1ec64632f05
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2013-12-03 22:09:46 -0500

    model associations

--- app/models/actum.rb
+++ app/models/actum.rb
@@ -2,6 +2,7 @@ class Actum < ActiveRecord::Base
   attr_accessible :alianza, :dc, :liberal, :libre, :nacional, :numero, :pac, :pinu, :ud, :nulos, :blancos, :user_id, :ready_for_review,:is_sum_ok, :actum_type,:image_changed
 
   validates :numero, :uniqueness=> {:scope => :actum_type}
+  belongs_to :municipio 
   
   validates :alianza, :dc, :liberal, :libre, :nacional, :pac, :pinu, :ud, :nulos, :blancos, :numericality => { :greater_than_or_equal_to=>0, :less_than_or_equal_to => 400 }, :presence => true
 
--- app/models/municipio.rb
+++ app/models/municipio.rb
@@ -1,4 +1,5 @@
 class Municipio < ActiveRecord::Base
   attr_accessible :name, :num, :departamento_id
   belongs_to :departamento
+  has_many :actas
 end
