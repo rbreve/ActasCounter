d7cb5c1ae77c54de165c6017938a6375ab71071a
Fernando Irías Escher <fernandoiriase@gmail.com>
2013-12-04 00:01:21 -0500

    Seed.rb para municipios y departamentos.

--- app/models/departamento.rb
+++ app/models/departamento.rb
@@ -1,4 +1,4 @@
 class Departamento < ActiveRecord::Base
-  attr_accessible :name, :num
+  attr_accessible :name, :num, :from_actum, :to_actum
   has_many :municipios
 end
--- app/models/municipio.rb
+++ app/models/municipio.rb
@@ -1,5 +1,5 @@
 class Municipio < ActiveRecord::Base
-  attr_accessible :name, :num, :departamento_id
+  attr_accessible :name, :num, :departamento_id, :from_actum, :to_actum
   belongs_to :departamento
   has_many :actas
 end
