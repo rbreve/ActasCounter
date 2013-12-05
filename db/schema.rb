aea92786106a468a6a4c45406d5301f211d580eb
Roberto Breve <rbreve@gmail.com>
2013-12-05 11:36:31 -0600

    rollback

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131205015940) do
+ActiveRecord::Schema.define(:version => 20131203040512) do
 
   create_table "acta", :force => true do |t|
     t.string   "numero"
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(:version => 20131205015940) do
     t.datetime "date"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "imagenActa"
   end
 
   create_table "users", :force => true do |t|
