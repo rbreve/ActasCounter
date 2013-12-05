fafd1d7bc9ee73163c294df462ef4a414b920182
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2013-12-05 08:49:23 -0500

    image uploader

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJBP6OA6EIVX7FGEA',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],                        # required
     :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
     :host                   => 'imagenesactas.s3-website-us-west-2.amazonaws.com',             # optional, defaults to nil
     :endpoint               => 'http://imagenesactas.s3-website-us-west-2.amazonaws.com' # optional, defaults to nil
