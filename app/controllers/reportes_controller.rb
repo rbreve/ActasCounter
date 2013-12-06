0a13ac392fc5a3e5f5af6728d760a433f09c3b64
Alejandro Corpeno <alejandro.corpeno@gmail.com>
2013-12-05 22:39:22 -0500

    remove routes and actions that will not be used for reports

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -1,6 +1,4 @@
 class ReportesController < ApplicationController
-  # GET /reportes
-  # GET /reportes.json
   def index
     @reportes = Reporte.order("created_at DESC").page(params[:page]).per_page(25)
 
@@ -10,35 +8,6 @@ class ReportesController < ApplicationController
     end
   end
 
-  # GET /reportes/1
-  # GET /reportes/1.json
-  def show
-    @reporte = Reporte.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @reporte }
-    end
-  end
-
-  # GET /reportes/new
-  # GET /reportes/new.json
-  def new
-    @reporte = Reporte.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @reporte }
-    end
-  end
-
-  # GET /reportes/1/edit
-  def edit
-    #@reporte = Reporte.find(params[:id])
-  end
-
-  # POST /reportes
-  # POST /reportes.json
   def create
     @reporte = Reporte.new(params[:reporte])
     @reporte.user_id=current_user.id
@@ -46,27 +15,13 @@ class ReportesController < ApplicationController
     
     respond_to do |format|
       if @reporte.save
-        @acta = @reporte.actum
-        format.html { redirect_to @acta, notice: 'Gracias, su reporte fue creado' }
+        format.html { redirect_to  @reporte.actum, notice: 'Gracias, su reporte fue creado' }
         format.json { render json: @reporte, status: :created, location: @reporte }
-        format.js {}
       else
         format.html { render action: "new" }
         format.json { render json: @reporte.errors, status: :unprocessable_entity }
-        format.js {}
       end
     end
   end
 
-  # PUT /reportes/1
-  # PUT /reportes/1.json
-  def update
-    #
-  end
-
-  # DELETE /reportes/1
-  # DELETE /reportes/1.json
-  def destroy
-    
-  end
 end
