544e786a7ca5c342605ece72246398132ae2fc9d
Luis Borjas <luisfborjas@gmail.com>
2017-11-29 11:51:16 -0600

    updates models for 2017

--- app/models/actum.rb
+++ app/models/actum.rb
@@ -1,10 +1,28 @@
 class Actum < ActiveRecord::Base
-  attr_accessible :alianza,:dc,:liberal,:libre,:nacional,:numero,:pac,:pinu,:ud,:faper,:nulos,:blancos,:user_id, :ready_for_review,:is_sum_ok,:actum_type,:image_changed
+  attr_accessible :alianza_patriotica,
+                  :dc,
+                  :liberal,
+                  :libre_pinu,
+                  :nacional,
+                  :pac,
+                  :ud,
+                  :frente_amplio,
+                  :vamos,
+                  :numero,:nulos,:blancos,:user_id,:ready_for_review,:is_sum_ok,:actum_type,:image_changed
 
   validates :numero, :uniqueness=> {:scope => :actum_type}
   belongs_to :municipio 
   
-  validates :alianza, :dc, :liberal, :libre, :nacional, :pac, :pinu, :ud, :nulos, :blancos,:faper, :numericality => { :greater_than_or_equal_to=>0, :less_than_or_equal_to => 400 }, :presence => true
+  validates :alianza_patriotica,
+            :dc,
+            :liberal,
+            :libre_pinu,
+            :nacional,
+            :pac,
+            :vamos,
+            :ud,
+            :frente_amplio,
+            :nulos, :blancos, :numericality => { :greater_than_or_equal_to=>0, :less_than_or_equal_to => 400 }, :presence => true
 
   belongs_to :user #, counter_cache: true
   has_many :verifications, class_name: "Verification",:foreign_key=>"acta_id"
@@ -36,7 +54,17 @@ class Actum < ActiveRecord::Base
   end
   
   def total_votes
- self.nacional.to_i+self.liberal.to_i+self.libre.to_i+self.ud.to_i+self.alianza.to_i+self.pinu.to_i+self.blancos.to_i+self.pac.to_i+self.nulos.to_i+self.dc.to_i
+    self.nacional.to_i+
+      self.liberal.to_i+
+      self.libre_pinu.to_i+
+      self.ud.to_i+
+      self.alianza_patriotica.to_i+
+      self.vamos.to_i+
+      self.frente_amplio.to_i+
+      self.blancos.to_i+
+      self.pac.to_i+
+      self.nulos.to_i+
+      self.dc.to_i
   end
 
   def percentage(party)
@@ -66,14 +94,14 @@ class Actum < ActiveRecord::Base
   def self.results
     return {
       total: Actum.count_all_votes,
-      alianza: Actum.where(:actum_type=>'p').sum("alianza"),
+      alianza_patriotica: Actum.where(:actum_type=>'p').sum("alianza_patriotica"),
       dc: Actum.where(:actum_type=>'p').sum("dc"),
       liberal: Actum.where(:actum_type=>'p').sum("liberal"),
-      libre: Actum.where(:actum_type=>'p').sum("libre"),
+      libre_pinu: Actum.where(:actum_type=>'p').sum("libre_pinu"),
       nacional: Actum.where(:actum_type=>'p').sum("nacional"),
       pac: Actum.where(:actum_type=>'p').where(:actum_type=>'p').sum("pac"),
-      pinu: Actum.sum("pinu"),
-      faper: Actum.where(:actum_type=>'p').sum("faper"),
+      vamos: Actum.sum("vamos"),
+      frente_amplio: Actum.where(:actum_type=>'p').sum("frente_amplio"),
       ud: Actum.where(:actum_type=>'p').sum("ud"),
       nulos: Actum.where(:actum_type=>'p').sum("nulos"),
       blancos: Actum.where(:actum_type=>'p').sum("blancos")
--- app/models/verification.rb
+++ app/models/verification.rb
@@ -1,12 +1,34 @@
 class Verification < ActiveRecord::Base
  
-  attr_accessible :liberal,:nacional,:libre,:pac,:ud,:dc,:alianza,:pinu,:faper,:blancos,:nulos,:is_valid,:is_sum_ok,:acta_id,:image_changed,:user_id
+  attr_accessible :liberal,
+                  :nacional,
+                  :libre_pinu,
+                  :pac,
+                  :ud,
+                  :dc,
+                  :alianza_patriotica,
+                  :vamos,
+                  :frente_amplio,
+                  :blancos,
+                  :nulos,
+                  :is_valid,:is_sum_ok,:acta_id,:image_changed,:user_id
   belongs_to :user #, counter_cache: true
   belongs_to :actum, class_name: "Actum",:foreign_key=>"acta_id"
    
   #validates :user_id, :uniqueness => {:scope => :acta_id}
   
-  validates :alianza, :dc, :liberal, :libre, :nacional, :pac, :pinu, :ud, :nulos, :blancos, :numericality => { :greater_than_or_equal_to=>0, :less_than_or_equal_to => 400 }, :presence => true
+  validates :alianza_patriotica,
+            :dc,
+            :liberal,
+            :libre_pinu,
+            :nacional,
+            :pac,
+            :vamos,
+            :ud,
+            :frente_amplio,
+            :nulos,
+            :blancos,
+            :numericality => { :greater_than_or_equal_to=>0, :less_than_or_equal_to => 400 }, :presence => true
     
   after_save :update_counters
   
